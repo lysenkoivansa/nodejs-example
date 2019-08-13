728164961f926fb38198d3671287c8db74b1ed9e
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-13 20:42:34 +0200

    Add default routes.

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {Component} from "react";
 import {Container} from '@material-ui/core';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export class App extends Component<{}, {}> {
   render(): JSX.Element {
     return (
-      <Container maxWidth="sm">
-        Hello, World!
-      </Container>
+      <Router>
+        <Container maxWidth="sm">
+          <Route path="/" exact component={() => <div>Hello, World!</div>}/>
+        </Container>
+      </Router>
     );
   }
 }
