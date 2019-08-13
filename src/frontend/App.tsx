cad4d9cee48b88a3ac7f84d2de64fc979e6967df
alyudin <alyudin@alyudin-rj-krakow.hw.griddynamics.net>
2019-08-13 12:09:38 +0200

    Initial commit.

---
+++ src/frontend/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Component} from "react";
+
+export class App extends Component<{}, {}> {
+  render(): JSX.Element {
+    return (
+      <div>
+        Hello, World!
+      </div>
+    );
+  }
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+module.exports = {
+  entry: path.resolve(__dirname, 'src/frontend/index.tsx'),
+
+  watch: true,
+
+  mode: 'development',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, './dist/'),
+    port: 3000
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?mimetype=image/svg+xml' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=img/[name].[ext]' },
+      { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        loaders: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader?sourceMap',
+          {
+            loader: 'postcss-loader',
+            options: {
+              options: {}
+            }
+          },
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      __DEV__: false
+    }),
+
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/frontend/index.html'),
+      inject: true
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
