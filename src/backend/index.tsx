993c4bab02a05d031c073acfee3c7c8473632c41
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-13 21:31:01 +0200

    Fixed server.

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
+});
+
 app.listen(port, () => {
   console.info(`Listening on port: ${port}`);
 });
